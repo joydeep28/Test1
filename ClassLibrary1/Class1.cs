f8794a2f010adc2eea6012960d99200e262ad54e
joydeep28 <joyrcc@gmail.com>
2016-07-31 09:53:32 +0530

    BaselineCode
    BaselineCode

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public abstract class Class1 : Interface2
+    {
+        public int test()
+        {
+            return 0;
+        }
+
+        public int test1()
+        {
+            return 1;
+        }
+
+        public abstract int test3();
+
+        public int test4()
+        {
+            return 4;
+        }
+    }
+
+    public class Class2: Class1
+    {
+        public override int test3()
+        {
+            return 3;
+        }
+       
+    }
+
+    public class Class3
+    {
+        public static void main(String[] args)
+        {
+            Class2 obj = new Class2();
+            Console.WriteLine(obj.test());
+        }
+    }
+}
---
+++ ClassLibrary1/Interface1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+   interface Interface1
+    {
+        int test4();
+        //int test1();
+
+    }
+
+    interface Interface2: Interface1
+    {
+       int test4();
+       
+    }
+
+}
